76c4e5ff11d123f26f0b006dd300ddb8a8e12e01
SukruYuksel <yksl3743dc@gmail.com>
2024-01-07 13:33:11 +0100

    Yeni  BH11 adinda Brans olusturuldu ve eklendi

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,7 @@
 public class Test2 {
 
     public static void main(String[] args) {
+        System.out.println("Sali günü görüsürüz");
         System.out.println("Iyi Pazarlar");
         System.out.println("Yarin Pazartesi");
     }
